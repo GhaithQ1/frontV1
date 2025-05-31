dbce87ec52d33fb5687dea5e21b8295e0758744e
GhaithQ1 <ghaithdrh@gmail.com>
2025-05-31 18:08:34 +0300

    first commit

---
+++ vite.config.js
@@ -0,0 +1,45 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'My Awesome App',
+        short_name: 'AwesomeApp',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+theme_color: '#f0edf5',
+        description: 'An interactive web application that works like a real application!',
+        icons: [
+          {
+            src: 'favicon-96x96.png',
+            sizes: '96x96',
+            type: 'image/png'
+          },
+          {
+            src: 'web-app-manifest-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'web-app-manifest-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ],
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': 'http://localhost:8000',
+    }
+  },
+});
